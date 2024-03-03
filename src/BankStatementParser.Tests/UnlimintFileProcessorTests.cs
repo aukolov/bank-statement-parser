eefb76e90e103574184d8cce2090acea46291abc
Alexey Ukolov <aukolov@gmail.com>
2024-03-03 15:50:27 +0200

    Unlimint - initial commit

---
+++ src/BankStatementParser.Tests/UnlimintFileProcessorTests.cs
@@ -0,0 +1,85 @@
+using System;
+using System.IO;
+using System.Linq;
+using BankStatementParser.Banks;
+using NUnit.Framework;
+using Shouldly;
+
+namespace BankStatementParser.Tests
+{
+    public class UnlimintFileProcessorTests
+    {
+        private UnlimintFileProcessor _fileProcessor;
+        private TransactionSerializer _transactionSerializer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileProcessor = new UnlimintFileProcessor();
+            _transactionSerializer = new TransactionSerializer();
+        }
+
+        private static string[] GetPdfFiles() =>
+            Directory.GetFiles("./test-data/revolut", "*.pdf");
+
+        [TestCaseSource(nameof(GetPdfFiles))]
+        public void ProcessesFiles(string pdfPath)
+        {
+            var statements = _fileProcessor.Process(pdfPath);
+
+            foreach (var grouping in statements.GroupBy(x => x.AccountNumber))
+            {
+                var csvFile = Path.Combine(
+                    Path.GetDirectoryName(pdfPath),
+                    Path.GetFileNameWithoutExtension(pdfPath) + "_" + grouping.Key + ".csv");
+                var expectedResult = File.ReadAllText(csvFile);
+                var actualResult = _transactionSerializer
+                    .Serialize(grouping.SelectMany(x => x.Transactions).ToArray());
+                actualResult.ShouldBe(expectedResult);
+            }
+        }
+        
+        [Test]
+        public void ExtractsFromAndToTimestamp()
+        {
+            var statements = _fileProcessor.Process("test-data/revolut/1.pdf");
+
+            statements.Length.ShouldBe(3);
+            statements[0].FromDate.ShouldBe(new DateTime(2023, 4, 1));
+            statements[0].ToDate.ShouldBe(new DateTime(2023, 6, 30));
+            statements[1].FromDate.ShouldBe(new DateTime(2023, 4, 1));
+            statements[1].ToDate.ShouldBe(new DateTime(2023, 6, 30));
+            statements[2].FromDate.ShouldBe(new DateTime(2023, 4, 1));
+            statements[2].ToDate.ShouldBe(new DateTime(2023, 6, 30));
+        }
+
+        [Test]
+        public void ExtractsAccountNumber()
+        {
+            var statements = _fileProcessor.Process("test-data/revolut/1.pdf");
+
+            foreach (var statement in statements)
+            {
+                statement.AccountNumber.ShouldStartWith("CY16");
+                statement.AccountNumber.ShouldEndWith("22");
+                statement.AccountNumber.Length.ShouldBe(25);
+            }
+        }
+
+        // [Test]
+        // public void ExtractsDifferentAccountNumbers()
+        // {
+        //     var statements = _fileProcessor.Process("test-data/revolut/2.pdf");
+
+        //     statements.Length.ShouldBe(4);
+        //     statements[0].AccountNumber.ShouldStartWith("Euro-LT");
+        //     statements[0].AccountNumber.ShouldEndWith("07");
+        //     statements[1].AccountNumber.ShouldStartWith("Vault-LT");
+        //     statements[1].AccountNumber.ShouldEndWith("99");
+        //     statements[2].AccountNumber.ShouldStartWith("British_Pound-LT");
+        //     statements[2].AccountNumber.ShouldEndWith("07");
+        //     statements[3].AccountNumber.ShouldStartWith("United_States_Dollar-LT");
+        //     statements[3].AccountNumber.ShouldEndWith("07");
+        // }
+    }
+}
---
+++ src/BankStatementParser/Banks/UnlimintFileProcessor.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using BankStatementParser.Extensions;
+using Pdf2Text;
+
+namespace BankStatementParser.Banks
+{
+    public class UnlimintFileProcessor : IFileProcessor
+    {
+        private readonly PdfParser _pdfParser = new PdfParser();
+        private readonly Regex _accountNumberRegex = new Regex(@"^\w{2}\d{10,}$");
+        private readonly Regex _trxnTypeRegex = new Regex(@"^\w{3}$");
+        private readonly Regex _amountRegex = new Regex(@"^(?<amount>(\d{1,3})([, ]\d{3})*\.\d{2}$)");
+
+        public Statement[] Process(string path)
+        {
+            var files = new List<string>();
+            if (Directory.Exists(path))
+            {
+                files.AddRange(
+                    Directory.GetFiles(path, "*.pdf", SearchOption.AllDirectories));
+                if (!files.Any())
+                    throw new Exception("No PDF files found.");
+            }
+            else if (File.Exists(path))
+            {
+                files.Add(path);
+            }
+
+            var statements = files.SelectMany(ProcessFile).ToArray();
+            return statements;
+        }
+
+        private Statement[] ProcessFile(string pdfPath)
+        {
+            var pdfModel = _pdfParser.Parse(pdfPath);
+
+            var state = State.SearchAccountNumber;
+            Transaction currentTrxn = null;
+            decimal? currentBalance = null;
+            var statements = new List<Statement>();
+            Statement statement = null;
+
+            var valueDateLeft = 0d;
+            var paymentDetailsLeft = 0d;
+            var beneficiaryLeft = 0d;
+            var debitRight = 0d;
+            var creditRight = 0d;
+
+            foreach (var page in pdfModel.Pages)
+            {
+                var nextPage = false;
+                if (page.Sentences.Count > 0 && page.Sentences[0].Text is "Monthly statement" or "Statement")
+                {
+                    if (currentTrxn != null && statement != null)
+                    {
+                        statement.Transactions.Add(currentTrxn);
+                    }
+                    statement = new Statement
+                    {
+                        Transactions = new List<Transaction>()
+                    };
+                    currentTrxn = null;
+                    currentBalance = null;
+                    debitRight = 0;
+                    creditRight = 0;
+                    state = State.SearchAccountNumber;
+                }
+
+                for (var i = 0; i < page.Sentences.Count - 1 && !nextPage; i++)
+                {
+                    var s = page.Sentences[i];
+
+                    var next = page.Sentences[i + 1];
+                    switch (state)
+                    {
+                        case State.SearchAccountNumber:
+                            if ((s.Text == "IBAN (SEPA)" || s.Text == "IBAN")
+                                && s.Left.IsApproximately(406))
+                            {
+                                if (!_accountNumberRegex.IsMatch(next.Text))
+                                    throw new Exception(
+                                        $"Account number has unexpected format: {next.Text}.");
+
+                                statement.AccountNumber += "-" + next.Text;
+                                state = State.SearchStatementPeriod;
+                            }
+
+                            break;
+                        case State.SearchStatementPeriod:
+                            if (s.Text.StartsWith("Period"))
+                            {
+                                var periodMatch = Regex.Match(s.Text,
+                                    @"from (?<from>.+) - (?<to>.+)");
+                                var fromText = periodMatch.Groups["from"].Captures[0].Value;
+                                var toText = periodMatch.Groups["to"].Captures[0].Value;
+                                var from = DateTime.ParseExact(fromText, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                                statement.FromDate = statement.FromDate == DateTime.MinValue
+                                    ? from
+                                    : from < statement.FromDate
+                                        ? from
+                                        : statement.FromDate;
+                                var to = DateTime.ParseExact(toText, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                                statement.ToDate = statement.ToDate == DateTime.MinValue
+                                    ? to
+                                    : to > statement.ToDate
+                                        ? to
+                                        : statement.ToDate;
+                                state = State.SearchValueDateColumn;
+                            }
+
+                            break;
+                        case State.SearchValueDateColumn:
+                            if (s.Text == "Value Date")
+                            {
+                                state = State.SearchPaymentDetailsColumn;
+                                valueDateLeft = s.Left;
+                            }
+
+                            break;
+                        case State.SearchPaymentDetailsColumn:
+                            if (s.Text == "Payment Details")
+                            {
+                                state = State.SearchBeneficiaryColumn;
+                                paymentDetailsLeft = s.Left;
+                            }
+
+                            break;
+                        case State.SearchBeneficiaryColumn:
+                            if (s.Text == "Remitter / Beneficiary")
+                            {
+                                state = State.SearchDebitColumn;
+                                beneficiaryLeft = s.Left;
+                            }
+
+                            break;
+                        case State.SearchDebitColumn:
+                            if (s.Text == "Debit")
+                            {
+                                state = State.SearchCreditColumn;
+                                debitRight = s.Right;
+                            }
+
+                            break;
+                        case State.SearchCreditColumn:
+                            if (s.Text != "Credit")
+                            {
+                                throw new Exception($"Money In column expected, but got '{s.Text}'.");
+                            }
+
+                            creditRight = s.Right;
+                            state = State.SearchTrxn;
+                            break;
+                        case State.SearchTrxn:
+                            if (DateTime.TryParseExact(s.Text, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None,  out _)
+                                && s.Left.IsApproximately(valueDateLeft))
+                            {
+                                if (currentTrxn != null)
+                                {
+                                    statement.Transactions.Add(currentTrxn);
+                                }
+
+                                currentTrxn = new Transaction
+                                {
+                                    Date = DateTime.ParseExact(s.Text, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None),
+                                    Description = ""
+                                };
+                                state = State.FirstDescription;
+                            }
+                            else if (s.Text.StartsWith("Created ") && s.Left.IsApproximately(valueDateLeft))
+                            {
+                                nextPage = true;
+                            }
+                            else if (s.Left.IsApproximately(paymentDetailsLeft)
+                                     && currentTrxn != null)
+                            {
+                                currentTrxn.Description += " " + s.Text;
+                            }
+
+                            break;
+                        case State.FirstDescription:
+                            if (currentTrxn == null)
+                                throw new Exception("Current transaction must not be null.");
+                            if (currentTrxn.Description.Length > 0)
+                                throw new Exception("Description was expected to be empty but was not.");
+
+                            currentTrxn.Description = s.Text;
+                            state = State.Amount;
+                            break;
+                        case State.Amount:
+                            var match = _amountRegex.Match(s.Text);
+                            if (!match.Success)
+                                throw new Exception($"Amount was expected but got {s.Text}.");
+                            if (currentTrxn == null)
+                                throw new Exception("Current transaction must not be null.");
+                            if (currentTrxn.Amount.HasValue)
+                                throw new Exception("Amount was expected to be null but was not.");
+
+                            var amount = decimal.Parse(match.Groups["amount"].Value.Replace(",", ""),
+                                CultureInfo.InvariantCulture);
+                            if (s.Right.IsApproximately(debitRight))
+                                currentTrxn.Amount = -amount;
+                            else if (s.Right.IsApproximately(creditRight))
+                                currentTrxn.Amount = +amount;
+                            else
+                                throw new Exception($"Found amount at unexpected location: {s.Right}.");
+                            state = State.SearchTrxn;
+
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+
+            if (currentTrxn != null)
+            {
+                statement.Transactions.Add(currentTrxn);
+            }
+
+            return statements.ToArray();
+        }
+
+        enum State
+        {
+            SearchAccountNumber,
+            SearchStatementPeriod,
+            SearchValueDateColumn,
+            SearchPaymentDetailsColumn,
+            SearchBeneficiaryColumn,
+            SearchDebitColumn,
+            SearchCreditColumn,
+            SearchTrxn,
+            FirstDescription,
+            Amount,
+            Balance,
+        }
+    }
+}
